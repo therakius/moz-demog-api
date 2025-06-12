3b3062b760664a294ea5ee3758ad826104430e04
Gaspar Mario Chichava <gasparc.chichava@gmail.com>
2025-06-12 09:31:57 +0200

    added provinces route

---
+++ index.js
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express"
+import morgan from "morgan";
+
+import provincesRoutes from "./src/routes/provincesRoutes.js";
+
+const port = 3000;
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json())
+
+
+app.use("/api/provinces", provincesRoutes)
+
+
+app.listen(port, ()=>{
+    console.log(`listening on port ${port}`)
+})
