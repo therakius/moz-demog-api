7f6c932e51ef692305bcb7cc14f5cb7dc4d82d07
Gaspar Mario Chichava <gasparc.chichava@gmail.com>
2025-06-14 17:42:46 +0200

    added route api/indicators/dependency-rate/:year

--- src/controllers/indicatorController.js
+++ src/controllers/indicatorController.js
@@ -69,3 +69,26 @@ export async function dependencyRate(req, res){
         res.status(500).json({info: 'internal server error'})
     }
 }
+
+export async function dependencyRatePerYear(req, res) {
+    const year = req.params.year
+    const query = `
+        select y.year, dr.*
+        from year y, dependency_rate dr
+        where y.year = $1
+    `
+
+    try {
+        const result = await db.query(query, [year])
+        
+        if(result.rowCount === 0) {
+            return res.status(404).json({info: 'Data not found'})
+        }
+
+        res.status(200).json({data: result.rows})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({info : 'internal server error'})
+    }
+    
+}
