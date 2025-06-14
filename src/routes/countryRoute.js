d99213287453bf0e3305c6fd973ee514ae5406f1
Gaspar Mario Chichava <gasparc.chichava@gmail.com>
2025-06-14 14:12:48 +0200

    added api/country:year for filtering data per year

--- src/routes/countryRoute.js
+++ src/routes/countryRoute.js
@@ -1,9 +1,10 @@
 import express from "express"
-import {getAllCountryData, getCountryData} from "../controllers/countryController.js";
+import {getAllCountryData, getCountryData, getCountryDataPerYear} from "../controllers/countryController.js";
 
 const router = express.Router();
 
 router.get("/all", getAllCountryData)
 router.get("/", getCountryData)
+router.get("/:year", getCountryDataPerYear)
 
 export default router;
