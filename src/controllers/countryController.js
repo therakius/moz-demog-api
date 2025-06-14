818e35ade42309a46da4300373caa545dfd73418
Gaspar Mario Chichava <gasparc.chichava@gmail.com>
2025-06-14 12:16:08 +0200

    created controller for country data

--- src/controllers/countryController.js
+++ src/controllers/countryController.js
@@ -1,8 +1,46 @@
 import db from "../models/db.js";
 
 export async function getCountryData (req, res) {
+    const query = `
+        SELECT
+        y.year,
+        y.head_of_state,
+
+        (
+            SELECT row_to_json(cd)
+            FROM country_data cd
+        ) AS country_data,
+
+
+        (
+            SELECT row_to_json(cpi)
+            FROM country_pop_indicators cpi
+            WHERE cpi.year_id = y.id
+        ) AS indicators,
+
+        (
+            SELECT row_to_json(dr)
+            FROM dependency_rate dr
+            WHERE dr.year_id = y.id
+        ) AS dependency_rate,
+
+        (
+            SELECT row_to_json(le)
+            FROM life_expectancy_at_birth le
+            WHERE le.year_id = y.id
+        ) AS life_expectancy,
+
+        (
+            SELECT row_to_json(im)
+            FROM infant_mortality im
+            WHERE im.year_id = y.id
+        ) AS infant_mortality
+
+        FROM year y;
+
+    `
     try {
-       const result = await db.query("select year.name, head_of_state, capital_city, official_language, TO_CHAR(independence_date, 'YYYY-MM-DD') AS independence_date , area_in_sqkm from year, country_data;")
+       const result = await db.query(query)
 
         if (result.rows.lenght === 0) {
             return res.status(404).json({erro: "Informacao nao encontrada"})
