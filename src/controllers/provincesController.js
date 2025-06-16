f102214d7d9453e245bccb1593b11eeb85cd1b2b
Gaspar Mario Chichava <gasparc.chichava@gmail.com>
2025-06-16 06:54:51 +0200

    fixed query issues in provinces' controller

--- src/controllers/provincesController.js
+++ src/controllers/provincesController.js
@@ -1,22 +1,50 @@
 import db from "../models/db.js";
 
 export async function getProvinces(req, res) {
+
+    const query = `
+    
+        select json_agg(
+        json_build_object(
+            'province_name', p.province_name,
+            'population_per_1000hab', row_to_json(ppt),
+            'population_percentual_structure', row_to_json(pps)
+            )            
+        ) province_data
+        from provinces p 
+        left join population_percentual_structure pps on p.id = pps.province_id
+        left join population_per_thousand ppt on p.id = ppt.province_id
+    
+    `
     try {
-        const result = await db.query('select * from provinces');
+        const result = await db.query(query);
         res.json(result.rows)
     } catch (error) {
         console.error(error);
-        res.status(500).json({error: "Erro ao buscar provincias"});
+        res.status(500).json({error: "Internal server error"});
     }
 }
 
 export async function getProvincesByName (req, res) {
     const name = req.params.name
-    console.log(name)
+
+    const query = `
+    
+        select json_agg(
+        json_build_object(
+            'province_name', p.province_name,
+            'population_per_1000hab', row_to_json(ppt),
+            'population_percentual_structure', row_to_json(pps)
+            )            
+        ) province_data
+        from provinces p 
+        left join population_percentual_structure pps on p.id = pps.province_id
+        left join population_per_thousand ppt on p.id = ppt.province_id
+        where province_name = $1
+    
+    `
     try {
-        const result = await db.query("select * from provinces where province_name = $1", 
-        [name]
-    );
+        const result = await db.query(query,[name]);
 
         if (result.rows.length === 0) return res.status(404).json({erro: "province not found"});
 
@@ -30,10 +58,15 @@ export async function getProvincesByName (req, res) {
 export async function percentualStructureByProvinceName(req, res) {
     const name = req.params.name
     const query = `
-        select p.province_name, pps.male_population, pps.female_population, total as average
-        from provinces p, population_percentual_structure pps
-        where p.id = pps.province_id
-        and p.province_name = $1;
+        select json_agg(
+        json_build_object(
+            'province_name', p.province_name,
+            'population_percentual_structure', row_to_json(pps)
+            )            
+        ) province_data
+        from provinces p 
+        left join population_percentual_structure pps on p.id = pps.province_id
+        where p.province_name = $1;
     `
 
     try {
@@ -53,10 +86,15 @@ export async function percentualStructureByProvinceName(req, res) {
 export async function perThousandByProvinceName(req, res) {
     const name = req.params.name
     const query = `
-        select p.province_name, ppt.per_thousand_male, ppt.per_thousand_female, ppt.per_thousand_total as total
-        from provinces p, population_per_thousand ppt
-        where p.id = ppt.province_id
-        and p.province_name = $1
+        select json_agg(
+        json_build_object(
+            'province_name', p.province_name,
+            'population_per_1000hab', row_to_json(ppt)
+            )            
+        ) province_data
+        from provinces p 
+        left join population_per_thousand ppt on p.id = ppt.province_id
+        where p.province_name = $1
         ;
     `
 
@@ -76,15 +114,29 @@ export async function perThousandByProvinceName(req, res) {
 
 export async function getProvincesbyYear(req, res){
     const year = req.params.year;
+    console.log(typeof year)
 
     const query = `
-        select y.year, p.*
-        from year y, provinces p
+    
+      select json_agg(
+        json_build_object(
+			'year', y.year,
+            'province_name', p.province_name,
+            'population_per_1000hab', row_to_json(ppt),
+            'population_percentual_structure', row_to_json(pps)
+            )            
+        ) province_data
+        from provinces p 
+        left join population_percentual_structure pps on p.id = pps.province_id
+        left join population_per_thousand ppt on p.id = ppt.province_id
+        left join year y on y.id = p.year_id
         where y.year = $1
-        and y.id = p.year_id;
     `
+    
 
-
+        if (Number(year) !== 2023) {
+        return res.status(404).json({info: 'Only data from 2023 is available for provinces'})
+    }
     try {
         const result = await db.query(query, [year])
         console.log(result)
@@ -102,7 +154,11 @@ export async function getAvailableYears(req, res) {
 
     const query = `
 
-        select y.year from year y;
+        select y.year
+        from year y
+        left join provinces p on y.id = p.year_id
+        where p.province_name is not null
+		group by y.year
     
     `
     try {
