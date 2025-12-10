f12f3dbc51507b9716746c09bc61e3ab7e054f38
Gaspar Mario Chichava <gasparc.chichava@gmail.com>
2025-12-10 19:15:01 +0200

    refractor: delete useless lines

--- src/routes/countryRoute.js
+++ src/routes/countryRoute.js
@@ -1,6 +1,5 @@
 import express from "express"
 import {getCountry} from "../controllers/countryController.js";
-import { getIndicators } from "../controllers/indicatorController.js";
 
 const router = express.Router();
 
