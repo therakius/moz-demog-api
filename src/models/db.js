336021f5f9acdbf8047b30a3511a9108bddcb78b
Gaspar Mario Chichava <gasparc.chichava@gmail.com>
2025-12-14 15:23:32 +0200

    used env variables for db connections

--- src/models/db.js
+++ src/models/db.js
@@ -11,11 +11,11 @@ dotenv.config();
 // });
 
 const db = new pg.Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "mozdemogapi",
-  password: "sic parvis magna",
-  port: 5432
+  user: process.env.DB_USER,
+  host:  process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
 })
 
 
