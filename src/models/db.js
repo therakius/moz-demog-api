8b1a7eec648b58a18f97fa7858b9ba898e761014
Gaspar Mario Chichava <gasparc.chichava@gmail.com>
2025-12-14 15:35:07 +0200

    updated db connection

--- src/models/db.js
+++ src/models/db.js
@@ -3,20 +3,20 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// const db = new pg.Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
-// });
-
 const db = new pg.Pool({
-  user: process.env.DB_USER,
-  host:  process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT
-})
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+// const db = new pg.Pool({
+//   user: process.env.DB_USER,
+//   host:  process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT
+// })
 
 
 db.connect()
