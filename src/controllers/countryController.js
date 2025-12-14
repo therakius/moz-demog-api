a3b1841264ec49a45c91b3ac616f8f97b38a36e1
Gaspar Mario Chichava <gasparc.chichava@gmail.com>
2025-12-14 17:27:50 +0200

    refractor: separated query from main function for country

--- src/controllers/countryController.js
+++ src/controllers/countryController.js
@@ -1,5 +1,6 @@
 import db from "../models/db.js";
 import {make_response} from "../../utils.js"
+import { makeCountryQuery } from "../models/countryModel.js";
 
 async function sendResponse(res, query, params = []) {
     try {
@@ -22,27 +23,12 @@ async function sendResponse(res, query, params = []) {
 
 export async function getCountry(req, res){
     const date = new Date();
-    const currentYear = date.getFullYear();
-    const year = req.query.year || currentYear;
     
-    const query = `
-        SELECT
-            JSON_BUILD_OBJECT(
-                    'country_name',
-                    'mozambique',
-                    'current_president', (select Y.HEAD_OF_STATE from public.year y where y.year = ${year}),
-                    'area',
-                    CD.TOTAL_AREA_SQKM,
-                    'capital_city',
-                    CD.CAPITAL_CITY,
-                    'independence_date',
-                    CD.INDEPENDENCE_DATE,
-                    'official_language',
-                    CD.OFFICIAL_LANGUAGE
-            ) as data
-        FROM
-            COUNTRY_DATA AS CD
-    `;
+    const currentYear = date.getFullYear();
+
+    const year = req.query.year || currentYear;
+
+    const query = makeCountryQuery(year)
 
     sendResponse(res, query);
 }
