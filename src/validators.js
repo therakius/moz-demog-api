3710ce73226d6d3b7b607b233247fa1833df6d2d
Gaspar Mario Chichava <gasparc.chichava@gmail.com>
2025-12-17 15:52:25 +0200

    feat: validations for fields in country/province-info

--- src/validators.js
+++ src/validators.js
@@ -3,6 +3,7 @@ import { provincesQuery } from "./controllers/provincesController.js";
 
 const regex = /[!@#$%^&*()\-+={}[\]:;"'<>,.?\/|\\]/;
 const regexArray = /[!@#$%^&*()\-+={}\:;'<>.?\/|\\]/;
+const regexDigits = /\d/;
 
 export function validateCountryFields(req) {
   let errors = {};
@@ -52,9 +53,8 @@ export async function validatePopulationFields(req) {
         field: "fields must not contain special characters",
       });
     }
-    if (!allowedFields.includes(field))
-        console.log(field)
-      Object.assign(errors, { [field]: `field '${field} is not allowed.`}); // uso de compute property name para as chaves dinamicas em objectos
+    if (!allowedFields.includes(field)) console.log(field);
+    Object.assign(errors, { [field]: `field '${field} is not allowed.` }); // uso de compute property name para as chaves dinamicas em objectos
   }
 
   if (year) {
@@ -69,8 +69,6 @@ export async function validatePopulationFields(req) {
     }
 
     if (p_name) {
-      console.log("BEFORE SEARCH: " + p_name);
-
       if (regex.test(p_name)) {
         return make_response(false, 400, {
           p_name: "fields must not contain special characters",
@@ -102,41 +100,37 @@ export function validateIndicatorsfields(req) {
   let message = "invalid input parameters";
 
   if (fields != null) {
-
-    if (fields.trim()==="") {
-        return make_response(false, 400, message, {
-          fields: "fields must not be empty.",
-        });
+    if (fields.trim() === "") {
+      return make_response(false, 400, message, {
+        fields: "fields must not be empty.",
+      });
     }
 
     if (regexArray.test(fields)) {
-        return make_response(false, 400, message, {
-          fields: "fields must not contain special characters.",
-        });
+      return make_response(false, 400, message, {
+        fields: "fields must not contain special characters.",
+      });
     }
 
     try {
-            
-        fields = JSON.parse(fields)
+      fields = JSON.parse(fields);
 
-        if(typeof(fields) != 'object') {
-            return make_response(false, 400, message, {
+      if (typeof fields != "object") {
+        return make_response(false, 400, message, {
           fields: "fields must be an array of strings.",
         });
-        }
-
+      }
     } catch (error) {
-        return make_response(false, 400, message, {
-          fields: "fields must be an array of strings.",
-        });
+      return make_response(false, 400, message, {
+        fields: "fields must be an array of strings.",
+      });
     }
 
-    if(fields.length ===0){
-        return make_response(false, 400, message, {
-          fields: "please fill the array with the desired filters.",
-        });
+    if (fields.length === 0) {
+      return make_response(false, 400, message, {
+        fields: "please fill the array with the desired filters.",
+      });
     }
-
 
     fields.forEach((f) => {
       if (!allowedFields.includes(f)) {
@@ -155,63 +149,93 @@ export function validateIndicatorsfields(req) {
     errors.y_start ??= [];
 
     if (y_start.trim() === "") {
-        errors.y_start.push("Must not be empty.");
+      errors.y_start.push("Must not be empty.");
     }
 
     if (regex.test(y_start)) {
-        errors.y_start.push("Must not contain special characters.");
+      errors.y_start.push("Must not contain special characters.");
     }
 
     if (isNaN(y_start)) {
-        errors.y_start.push("Must be a numeric value.");
+      errors.y_start.push("Must be a numeric value.");
     }
 
     const year = Number(y_start);
     if (!isNaN(year) && (year < 2017 || year > 2026)) {
-        errors.y_start.push("The year range must be 2017 - 2026.");
+      errors.y_start.push("The year range must be 2017 - 2026.");
     }
 
     if (errors.y_start.length === 0) {
-        delete errors.y_start;
+      delete errors.y_start;
     }
-}
-
-
-
+  }
 
   if (y_end != null) {
-
-    errors.y_end ??=[]
+    errors.y_end ??= [];
 
     if (y_end.trim() === "") {
-        errors.y_end.push("y_end must not be empty.")
-      }
+      errors.y_end.push("y_end must not be empty.");
+    }
 
     if (regex.test(y_end)) {
-        errors.y_end.push("y_start must not contain special characters.")
+      errors.y_end.push("y_start must not contain special characters.");
     }
 
-    if(isNaN(y_end)){
-        errors.y_end.push("y_end must be a numeric value..")
+    if (isNaN(y_end)) {
+      errors.y_end.push("y_end must be a numeric value..");
     }
 
-    if (Number(y_end) < 2017 || Number(y_end) > 2026){
-        errors.y_end.push("the year range must be 2017 - 2026.")
+    if (Number(y_end) < 2017 || Number(y_end) > 2026) {
+      errors.y_end.push("the year range must be 2017 - 2026.");
     }
 
-    if (errors.y_end.length === 0){
-        delete errors.y_end;
+    if (errors.y_end.length === 0) {
+      delete errors.y_end;
     }
   }
-
 
   if (y_start && y_end) {
-
-    if(y_start > y_end){
-        Object.assign(errors, { "y_end": "y_start must be greater than y_end" });
+    if (y_start > y_end) {
+      Object.assign(errors, { y_end: "y_start must be greater than y_end" });
     }
   }
 
+  if (Object.keys(errors).length > 0) {
+    return make_response(false, 400, message, errors, {});
+  }
+}
+
+export async function validateprovincesField(req) {
+  let errors = {};
+  let pExists = null;
+  const { p_name } = req.query;
+  const message = "Invalid input parameters";
+
+  if (p_name != null) {
+    errors.p_name ??= [];
+
+    if (p_name.trim() == "") {
+      errors.p_name.push("Must not be empty");
+    }
+
+    if (regex.test(p_name)) {
+      errors.p_name.push("Must not contain special characters");
+    }
+
+    if (regexDigits.test(p_name)) {
+      errors.p_name.push("Must not contain digits");
+    }
+
+    pExists = await provincesQuery(p_name);
+
+    if (pExists === 0) {
+      errors.p_name.push(`Unknown province name ${p_name}`);
+    }
+
+    if (errors.p_name.length === 0) {
+      delete errors.p_name;
+    }
+  }
 
   if (Object.keys(errors).length > 0) {
     return make_response(false, 400, message, errors, {});
