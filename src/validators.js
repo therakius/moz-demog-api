e1aae2abaa9393185749afbf2af59ea873a7d97c
Gaspar Mario Chichava <gasparc.chichava@gmail.com>
2025-12-17 15:07:52 +0200

    feat: added input validations for fields in country/indicators

--- src/validators.js
+++ src/validators.js
@@ -2,6 +2,7 @@ import { make_response } from "../utils.js";
 import { provincesQuery } from "./controllers/provincesController.js";
 
 const regex = /[!@#$%^&*()\-+={}[\]:;"'<>,.?\/|\\]/;
+const regexArray = /[!@#$%^&*()\-+={}\:;'<>.?\/|\\]/;
 
 export function validateCountryFields(req) {
   let errors = {};
@@ -52,7 +53,8 @@ export async function validatePopulationFields(req) {
       });
     }
     if (!allowedFields.includes(field))
-      Object.assign(errors, { [field]: `field ${field} is not allowed` }); // uso de compute property name para as chaves dinamicas em objectos
+        console.log(field)
+      Object.assign(errors, { [field]: `field '${field} is not allowed.`}); // uso de compute property name para as chaves dinamicas em objectos
   }
 
   if (year) {
@@ -74,11 +76,10 @@ export async function validatePopulationFields(req) {
           p_name: "fields must not contain special characters",
         });
       }
-      
+
       const pExists = await provincesQuery(p_name);
-      console.log("pExists: " + pExists);
+
       if (pExists === 0) {
-        console.log("ITS ZEERO");
         Object.assign(errors, { [p_name]: `Unknown province -- ${p_name}.` });
       }
     }
@@ -88,5 +89,131 @@ export async function validatePopulationFields(req) {
       console.log(make_response(false, 400, message, errors, {}));
       return make_response(false, 400, message, errors, {});
     }
+  }
+}
+
+export function validateIndicatorsfields(req) {
+  const allowedFields = ["p_structure", "d_rate", "p_thousand", "i_mortality"];
+
+  let { fields, y_start, y_end } = req.query;
+
+  let errors = {};
+
+  let message = "invalid input parameters";
+
+  if (fields != null) {
+
+    if (fields.trim()==="") {
+        return make_response(false, 400, message, {
+          fields: "fields must not be empty.",
+        });
+    }
+
+    if (regexArray.test(fields)) {
+        return make_response(false, 400, message, {
+          fields: "fields must not contain special characters.",
+        });
+    }
+
+    try {
+            
+        fields = JSON.parse(fields)
+
+        if(typeof(fields) != 'object') {
+            return make_response(false, 400, message, {
+          fields: "fields must be an array of strings.",
+        });
+        }
+
+    } catch (error) {
+        return make_response(false, 400, message, {
+          fields: "fields must be an array of strings.",
+        });
+    }
+
+    if(fields.length ===0){
+        return make_response(false, 400, message, {
+          fields: "please fill the array with the desired filters.",
+        });
+    }
+
+
+    fields.forEach((f) => {
+      if (!allowedFields.includes(f)) {
+        Object.assign(errors, { fields: `field '${f}' is not allowed.` });
+      }
+
+      if (regex.test(f)) {
+        Object.assign(errors, {
+          fields: `fields must not contain special characters (${f})`,
+        });
+      }
+    });
+  }
+
+  if (y_start != null) {
+    errors.y_start ??= [];
+
+    if (y_start.trim() === "") {
+        errors.y_start.push("Must not be empty.");
+    }
+
+    if (regex.test(y_start)) {
+        errors.y_start.push("Must not contain special characters.");
+    }
+
+    if (isNaN(y_start)) {
+        errors.y_start.push("Must be a numeric value.");
+    }
+
+    const year = Number(y_start);
+    if (!isNaN(year) && (year < 2017 || year > 2026)) {
+        errors.y_start.push("The year range must be 2017 - 2026.");
+    }
+
+    if (errors.y_start.length === 0) {
+        delete errors.y_start;
+    }
+}
+
+
+
+
+  if (y_end != null) {
+
+    errors.y_end ??=[]
+
+    if (y_end.trim() === "") {
+        errors.y_end.push("y_end must not be empty.")
+      }
+
+    if (regex.test(y_end)) {
+        errors.y_end.push("y_start must not contain special characters.")
+    }
+
+    if(isNaN(y_end)){
+        errors.y_end.push("y_end must be a numeric value..")
+    }
+
+    if (Number(y_end) < 2017 || Number(y_end) > 2026){
+        errors.y_end.push("the year range must be 2017 - 2026.")
+    }
+
+    if (errors.y_end.length === 0){
+        delete errors.y_end;
+    }
+  }
+
+
+  if (y_start && y_end) {
+
+    if(y_start > y_end){
+        Object.assign(errors, { "y_end": "y_start must be greater than y_end" });
+    }
+  }
+
+
+  if (Object.keys(errors).length > 0) {
+    return make_response(false, 400, message, errors, {});
   }
 }
