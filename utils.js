7382ce7b42c58c64947df80fda3b8fcb72c80028
Gaspar Mario Chichava <gasparc.chichava@gmail.com>
2026-02-23 13:41:06 +0200

    feat(auth+key): create function to compare hash + generate key

--- utils.js
+++ utils.js
@@ -1,3 +1,5 @@
+import bcrypt from "bcryptjs"
+import { generateApiKey } from "generate-api-key"
 
 export function make_response(success, status, message, errors=[], data, metadata) {
     return {
@@ -9,3 +11,23 @@ export function make_response(success, status, message, errors=[], data, metadat
         metadata
     }
 }
+
+
+const salt = bcrypt.genSaltSync(10)
+export function hashPassword(password) {
+    const hash = bcrypt.hashSync(password, salt)
+    return hash
+}
+
+export async function comparePasswords(password, dbHash){    
+    const isValid = await bcrypt.compare(password, dbHash);
+
+    return isValid;
+}
+
+export const keyGenerator = ()=>{
+    const prefix = "mz"
+    const key = prefix+generateApiKey({length: 32, method: "base62"})
+    return key
+}
+
