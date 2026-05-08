f069413919b7688daa5c71de0de75febdd8fd535
Gaspar Mario Chichava <gasparc.chichava@gmail.com>
2026-05-08 17:07:04 +0200

    feat (validator+indicators): validate array

--- src/validators.js
+++ src/validators.js
@@ -112,6 +112,11 @@ export function validateIndicatorsfields(req) {
       if (typeof fields != "object") {
         fields = JSON.parse(fields);
       }
+
+      if (typeof fields == "string" || !Array.isArray(fields)) {
+        throw new Error("fields must be an array of strings.");
+      }
+      
     } catch (error) {
       return make_response(false, 400, message, {
         fields: "fields must be an array of strings.",
