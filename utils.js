2518cca43ca41ad58943a2801596c3149a2c7093
Gaspar Mario Chichava <gasparc.chichava@gmail.com>
2026-06-07 21:31:10 +0200

    chore: refractoring code in utils

--- utils.js
+++ utils.js
@@ -1,45 +1,48 @@
-import bcrypt from "bcryptjs"
-import { generateApiKey } from "generate-api-key"
+import bcrypt from "bcryptjs";
+import { generateApiKey } from "generate-api-key";
 
-export function make_response(success, status, message, errors=[], data, metadata) {
-    return {
-        success,
-        status,
-        message,
-        errors,
-        data,
-        metadata
-    }
+export function make_response(
+  success,
+  status,
+  message,
+  errors = [],
+  data,
+  metadata,
+) {
+  return {
+    success,
+    status,
+    message,
+    errors,
+    data,
+    metadata,
+  };
 }
 
-
-const salt = bcrypt.genSaltSync(10)
+const salt = bcrypt.genSaltSync(10);
 export function hashPassword(password) {
-    const hash = bcrypt.hashSync(password, salt)
-    return hash
+  const hash = bcrypt.hashSync(password, salt);
+  return hash;
 }
 
-export async function comparePasswords(password, dbHash){    
-    const isValid = await bcrypt.compare(password, dbHash);
+export async function comparePasswords(password, dbHash) {
+  const isValid = await bcrypt.compare(password, dbHash);
 
-    return isValid;
+  return isValid;
 }
 
-export const keyGenerator = (type)=>{
+export const keyGenerator = (type) => {
+  let prefix;
 
-    let prefix;
-    
-    if (type === 'key') {
-        prefix = "mz"
-    }
+  if (type === "key") {
+    prefix = "mz";
+  }
 
-    if (type ==='pass') {
-        prefix =  "cred"
-    }
+  if (type === "pass") {
+    prefix = "cred";
+  }
 
-    const hash = prefix + generateApiKey({length: 32, method: "base62"})
-    
-    return hash
-}
+  const hash = prefix + generateApiKey({ length: 32, method: "base62" });
 
-
+  return hash;
+};
